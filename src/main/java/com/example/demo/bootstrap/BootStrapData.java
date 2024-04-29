a95328a7e4c5681dfc306fc2a4814b61ccddc605
lindavataksi <your.email@example.com>
2024-04-28 23:24:11 -0400

    Add 10 new parts/products (lines 42-90, 96-110) with duplicate product prevention using an if-statement. Sample inventory is added only when both part and product lists are empty.

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -39,33 +39,74 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-       /*
-        OutsourcedPart o= new OutsourcedPart();
-        o.setCompanyName("Western Governors University");
-        o.setName("out test");
-        o.setInv(5);
-        o.setPrice(20.0);
-        o.setId(100L);
-        outsourcedPartRepository.save(o);
-        OutsourcedPart thePart=null;
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            if(part.getName().equals("out test"))thePart=part;
+        if (partRepository.count() == 0) {
+            OutsourcedPart clasp = new OutsourcedPart();
+            clasp.setCompanyName("Pandora");
+            clasp.setName("Clasp");
+            clasp.setInv(250);
+            clasp.setPrice(10.0);
+            clasp.setId(100L);
+            outsourcedPartRepository.save(clasp);
+            OutsourcedPart thePart = null;
+            List<OutsourcedPart> outsourcedParts = (List<OutsourcedPart>) outsourcedPartRepository.findAll();
+    //        for(OutsourcedPart part:outsourcedParts){
+    //            if(part.getName().equals("out test"))thePart=part;
+    //        }
+    //
+    //        System.out.println(thePart.getCompanyName());
+    
+            OutsourcedPart chain = new OutsourcedPart();
+            chain.setCompanyName("Pandora");
+            chain.setName("Chain");
+            chain.setInv(25);
+            chain.setPrice(300.0);
+            chain.setId(200L);
+            outsourcedPartRepository.save(chain);
+    
+            OutsourcedPart pendant = new OutsourcedPart();
+            pendant.setCompanyName("Pandora");
+            pendant.setName("Pendant");
+            pendant.setInv(50);
+            pendant.setPrice(45.0);
+            pendant.setId(300L);
+            outsourcedPartRepository.save(pendant);
+    
+            OutsourcedPart gemstone = new OutsourcedPart();
+            gemstone.setCompanyName("Pandora");
+            gemstone.setName("Gemstone");
+            gemstone.setInv(50);
+            gemstone.setPrice(45.0);
+            gemstone.setId(400L);
+            outsourcedPartRepository.save(gemstone);
+    
+            OutsourcedPart diamond = new OutsourcedPart();
+            diamond.setCompanyName("Pandora");
+            diamond.setName("Diamond");
+            diamond.setInv(10);
+            diamond.setPrice(1000.0);
+            diamond.setId(500L);
+            outsourcedPartRepository.save(diamond);
         }
-
-        System.out.println(thePart.getCompanyName());
-        */
+    
         List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
         for(OutsourcedPart part:outsourcedParts){
             System.out.println(part.getName()+" "+part.getCompanyName());
         }
 
-        /*
-        Product bicycle= new Product("bicycle",100.0,15);
-        Product unicycle= new Product("unicycle",100.0,15);
-        productRepository.save(bicycle);
-        productRepository.save(unicycle);
-        */
+        Product ring = new Product("Ring", 500.0, 35);
+        Product necklace = new Product("Necklace", 1000.0, 90);
+        Product bracelet = new Product("Bracelet", 175.0, 200);
+        Product anklet= new Product("Anklet", 220.0, 4);
+        Product earring = new Product("Earring", 150.0, 6);
+        if (productRepository.count() == 0) {
+            productRepository.save(ring);
+            productRepository.save(necklace);
+            productRepository.save(bracelet);
+            productRepository.save(anklet);
+            productRepository.save(earring);
+        }
+
+
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
